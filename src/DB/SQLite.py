22823ac6cb32c856ec37aa841063434eaf37e5a8
francescoreconditi <francesco.reconditi@gmail.com>
2022-02-09 17:23:02 +0100

    .

--- src/DB/SQLite.py
+++ src/DB/SQLite.py
@@ -1,8 +1,16 @@
+#
+# fff Import
+#
+
 import logging
 import sqlite3
 import sys
 import traceback
 from contextlib import contextmanager
+
+#
+# fff Funzioni
+#
 
 
 @contextmanager
@@ -15,23 +23,32 @@ def open_db(db_file: str):
     Yields:
         'cursore sqlite': Cursore creato dalla Connessione
     """
+    # ! Tiro su la connessione
     conn = sqlite3.connect(db_file)
 
     try:
-        logging.info("Creazione Connessione ...")
+        logging.info("-- Creazione Connessione --")
+
+        # ! "preparo" il cursore per l' uso successivo
         yield conn.cursor()
 
     except sqlite3.Error as err_sql:
-        logging.error(('SQLite error: %s' % (' '.join(err_sql.args))))
-        logging.error(f"Exception class is: {err_sql.__class__}")
-        logging.error('SQLite traceback: ')
+        # ! Errore, prendo tutte le informazioni
+        logging.error(('SQLite error  : %s' % (' '.join(err_sql.args))))
+        logging.error(f"  Class       : {err_sql.__class__}")
+        logging.error("  Traceback   : ")
         exc_type, exc_value, exc_tb = sys.exc_info()
         logging.error(traceback.format_exception(exc_type, exc_value, exc_tb))
 
     finally:
-        logging.info("Chiusura Connessione ...")
+        # ! Comunque vada chiudo tutto
+        logging.info("-- Chiusura Connessione --")
         conn.commit()
         conn.close()
+
+#
+# fff main
+#
 
 
 def main():
